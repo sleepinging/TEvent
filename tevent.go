0c5c6a8c6242892776c998c01ae7b1fc7b12e6b0
知 未 <i237731947@outlook.com>
2018-06-13 13:23:19 +0800

    测试

--- tevent.go
+++ tevent.go
@@ -16,6 +16,9 @@ func getArgs(args ...interface{}) []reflect.Value {
 	return ma
 }
 
+//某事件发生
+//eid 事件id
+//args 参数
 func (this *TEvent) Happen(eid int, args ...interface{}) {
 	fs := this.EventList[eid]
 	if len(fs) == 0 {
@@ -27,10 +30,16 @@ func (this *TEvent) Happen(eid int, args ...interface{}) {
 	}
 }
 
+//添加事件处理函数
+//eid 事件id
+//fh 事件处理函数
 func (this *TEvent) AddEventHandler(eid int, fh EventFunc) {
 	this.EventList[eid] = append(this.EventList[eid], fh)
 }
 
+//移除事件处理函数
+//eid 事件id
+//fh 事件处理函数
 func (this *TEvent) ReMoveEventHandler(eid int, fh EventFunc) {
 	fs := this.EventList[eid]
 	if len(fs) == 0 {
@@ -44,12 +53,12 @@ func (this *TEvent) ReMoveEventHandler(eid int, fh EventFunc) {
 	}
 }
 
-func (this *TEvent) Init() {
+func (this *TEvent) init() {
 	this.EventList = make(map[int][]EventFunc)
 }
 
 func NewEvent() (e *TEvent) {
 	e = new(TEvent)
-	e.Init()
+	e.init()
 	return
 }
